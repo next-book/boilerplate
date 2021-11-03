60b3da080faecff54cf9d1d728db4158ead49691
jan-martinek <honza.martinek@gmail.com>
2021-11-03 23:44:42 +0000

    Deploying to docs from @ next-book/boilerplate@c5358091dbe02b6ba31aa7cbaf366cbd3314811b 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-4c5832d';
+const CACHE = 'cache-c535809';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
