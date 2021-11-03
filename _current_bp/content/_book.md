99b5699f70a2104ad43aa1c0d13729831e03db53
jan-martinek <jan-martinek@users.noreply.github.com>
2021-11-03 23:40:21 +0000

    Deploying to main from @ next-book/boilerplate@4c5832d46ddd10da7a86efa4780a1ad23236b01b 🚀

---
+++ _current_bp/content/_book.md
@@ -0,0 +1,23 @@
+---
+outputs: meta
+slug: book
+
+languageCode: en-US
+meta:
+  title: "Next-book Boilerplate"
+  author: "Author Name"
+  published: 2020
+  keywords: []
+chapters:
+  - where-i-lived-and-what-i-lived-for.html
+  - reading.html
+  - sounds.html
+  - solitude.html
+static:
+  - style
+  - scripts
+  - fonts
+  - images
+  - favicon.png
+---
+
---
+++ _current_bp/filters.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const book = JSON.parse(fs.readFileSync("./_temp/book.json", "utf8"));
+
+book.chapters.forEach(filename => {
+  const path = "./_temp/" + filename;
+  const text = fs.readFileSync(path, "utf8");
+  fs.writeFileSync(
+    path,
+    text
+      .replace(/(<table[^>]*>)/g, '<div class="table-wrapper">$1')
+      .replace(/(<\/table>)/g, "$1</div>")
+  );
+});
+
+console.log("Filters applied…");
